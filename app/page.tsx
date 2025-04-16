617946ab5537ccac6e8a19e6a159f5f579956173
Ruslan Gainutdinov <ruslan@wizecore.com>
2025-04-16 10:06:29 +0300

    fix: logo

--- app/page.tsx
+++ app/page.tsx
@@ -28,16 +28,16 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://dollardeploy.com/?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="https://dollardeploy.com/icon.svg"
+              alt="DollarDeploy logomark"
+              width={32}
+              height={32}
             />
             Deploy now
           </a>
